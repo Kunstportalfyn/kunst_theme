e8503d68471f9cac1414a7a3ac032b5a9ec36a8b
gorkha <anme@odense.dk>
2013-09-18 15:30:05 +0200

    Fixed logout and user buttons

--- js/base.js
+++ js/base.js
@@ -36,10 +36,10 @@
     $('#block-search-form').prepend(html);
   }
   function showLogoutButton(){
-    var onclick = "location.href='/user/logout'";
-    var html = '<button name="log-out-button" type="button" onclick="' + onclick + '" title="' + Drupal.t("Logout") + '"><i class="icon-signout"> </i></button>';
-    var onclick = "location.href='/user/'";
-    html += '<button name="logged-in-user" type="button" onclick="' + onclick + '" title="' + Drupal.t("User") + '"><i class="icon-user"> </i></button>';
+    var url = "/user/logout";
+    var html = '<a href="' + url + '"><button name="log-out-button" type="button" title="' + Drupal.t("Logout") + '"><i class="icon-signout"> </i></button></a>';
+    var url = "/user/";
+    html += '<a href="' + url + '"><button name="logged-in-user" type="button" title="' + Drupal.t("User") + '"><i class="icon-user"> </i></button></a>';
     $('#block-search-form').prepend(html);
   }
 

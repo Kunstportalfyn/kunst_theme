2055615bc53bd10321dcafd409f1611c78b0735a
gorkha <anme@odense.dk>
2013-09-18 15:21:41 +0200

    Responsive design: Added resize event handler for window size below 768

--- js/base.js
+++ js/base.js
@@ -33,11 +33,45 @@
   function showLoginButton(){
     var onclick = "jQuery.fn.loginBox()";
     var html = '<button type="button" onclick="' + onclick + '">' + Drupal.t("Login") + '</button>';
-    $('#block-search-form').prepend(html);//.wrap('<div class="position_wrapper"/>');
+    $('#block-search-form').prepend(html);
+  }
+  function showLogoutButton(){
+    var onclick = "location.href='/user/logout'";
+    var html = '<button name="log-out-button" type="button" onclick="' + onclick + '" title="' + Drupal.t("Logout") + '"><i class="icon-signout"> </i></button>';
+    var onclick = "location.href='/user/'";
+    html += '<button name="logged-in-user" type="button" onclick="' + onclick + '" title="' + Drupal.t("User") + '"><i class="icon-user"> </i></button>';
+    $('#block-search-form').prepend(html);
+  }
+
+  var tmp = false;
+
+  function setSearchFieldWidth() {
+    if((d = $(window).width())<768) {
+      var boxWidth = $("#header").outerWidth();
+      var editBoxWidth = boxWidth - 130;
+      $(".region-header #block-search-form .form-item-search-block-form .form-text").width(editBoxWidth);
+      tmp = true;
+    } else {
+      if(tmp == true) {
+        $(".region-header #block-search-form .form-item-search-block-form .form-text").attr('style', "");
+      }
+    }
+    console.log(d);
   }
 
   $().ready(function(){
     loginFormWrap();
-    showLoginButton();
+    // check if user is logged in
+    if($("#block-user-login").length) {
+      showLoginButton();
+    }
+    else {
+      showLogoutButton();
+    }
+    setSearchFieldWidth();
+  });
+  
+  $(window).resize(function() {
+    setSearchFieldWidth();
   });
 })(jQuery);

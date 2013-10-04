58add185b00c8ccfc1a3bbf5d4d9cc5dfa8755cb
gorkha <anme@odense.dk>
2013-10-04 14:33:13 +0200

    Removed some of the double mouse enter/leave events by fixing sub menu position

--- js/menu.js
+++ js/menu.js
@@ -4,20 +4,20 @@
  */
 
 (function($) {
-  $(document).ready(function(){
+  $(document).ready(function() {
     adjustMenu();
   });
   
-  $(window).resize(function(){
+  $(window).resize(function() {
     adjustMenu();
   });
   
-  function adjustMenu() {
+  function adjustMenu(e) {
     if($(window).width() > 767) {
-      $("#block-menu-block-1 ul.menu > li").mouseenter(function(){
+      $("#block-menu-block-1 ul.menu > li").mouseenter(function(e) {
         $(this).find("ul li").slideDown(100);
       });
-      $("#block-menu-block-1 ul.menu > li").mouseleave(function(){
+      $("#block-menu-block-1 ul.menu > li").mouseleave(function(e) {
         $(this).find("ul li").slideUp(100);
       });
       console.log("Wide");

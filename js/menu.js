c3981f33ea40f2570b1c2462fb4b7395c759aba0
gorkha <anme@odense.dk>
2013-10-07 10:19:54 +0200

    Fixed some colors

--- js/menu.js
+++ js/menu.js
@@ -15,10 +15,10 @@
   function adjustMenu(e) {
     if($(window).width() > 767) {
       $("#block-menu-block-1 ul.menu > li").mouseenter(function(e) {
-        $(this).find("ul li").slideDown(100);
+        $(this).find("ul li").stop(true, true).slideDown(100);
       });
       $("#block-menu-block-1 ul.menu > li").mouseleave(function(e) {
-        $(this).find("ul li").slideUp(100);
+        $(this).find("ul li").stop(true, true).slideUp(100);
       });
       console.log("Wide");
     }

8f273529f721fc378d4f9c9024fe6a8eb19c98f6
gorkha <anme@odense.dk>
2013-10-07 14:25:16 +0200

    Fix for IE8 . Requires Conditional Stylesheets module

--- js/menu.js
+++ js/menu.js
@@ -5,12 +5,24 @@
 
 (function($) {
   $(document).ready(function() {
+    addExpandable();
     adjustMenu();
   });
   
   $(window).resize(function() {
     adjustMenu();
   });
+  
+  function addExpandable() {
+    var div = '<div class="expandable"><i class="icon-caret-down"> </i></div>';
+    var $aArr = $("#block-menu-block-1 > div.content > div > ul.menu > li");
+    if($aArr.length === 0) $aArr = $("#rm-removed > div.content > div > ul.menu > li");
+    $aArr.each(function(i, a){
+      $a = $(a).find(">a");
+      console.log($a.html());
+      $a.html($a.html() + div);
+    });
+  }
   
   function adjustMenu(e) {
     if($(window).width() > 767) {
@@ -20,12 +32,11 @@
       $("#block-menu-block-1 ul.menu > li").mouseleave(function(e) {
         $(this).find("ul li").stop(true, true).slideUp(100);
       });
-      console.log("Wide");
+      //console.log("Wide");
     }
     else {
-      console.log("Mobile");
+      //console.log("Mobile");
       $("#block-menu-block-1 ul.menu > li").unbind();
-      //$("#rm-removed li").css("display", "list-item");
       $("#rm-removed li").attr("style","");
     }
   }

c5716adf07cdbeb7b22d6b8a97fc785798580914
gorkha <anme@odense.dk>
2013-10-11 14:45:59 +0200

    Fixed 3rd level menu items in main menu

--- js/menu.js
+++ js/menu.js
@@ -15,19 +15,19 @@
   
   function addExpandable() {
     var div = '<div class="expandable"><i class="icon-caret-down"> </i></div>';
-    var $aArr = $("#block-menu-block-1 > div.content > div > ul.menu > li");
-    if($aArr.length === 0) $aArr = $("#rm-removed > div.content > div > ul.menu > li");
+    var $aArr = $("#block-menu-block-1 ul.menu > li");
+    if($aArr.length === 0) $aArr = $("#rm-removed ul.menu > li");
     $aArr.each(function(i, a){
       $a = $(a).find(">a");
       console.log($a.html());
-      $a.html($a.html() + div);
+      $a.html(div + $a.html());
     });
   }
   
   function adjustMenu(e) {
     if($(window).width() > 767) {
       $("#block-menu-block-1 ul.menu > li").mouseenter(function(e) {
-        $(this).find("ul li").stop(true, true).slideDown(100);
+        $(this).find("> ul > li").stop(true, true).slideDown(100);
       });
       $("#block-menu-block-1 ul.menu > li").mouseleave(function(e) {
         $(this).find("ul li").stop(true, true).slideUp(100);

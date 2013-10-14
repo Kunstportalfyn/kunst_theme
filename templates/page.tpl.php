f8183d7352b210ca04d16dca6c371e1e32d4d8b4
gorkha <anme@odense.dk>
2013-10-14 15:44:22 +0200

    Removed: background-image Added: shadow-wrapper Added: margin-left on social profile

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -63,43 +63,45 @@
  */
 ?>
 <div id="wrapper">
-  <header id="header" class="clearfix">
-    <?php if (theme_get_setting('image_logo','kunst_theme')): ?>
-      <?php if ($logo): ?><div id="site-logo"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php print render($page['header']); ?></div><?php endif; ?>
-    <?php else: ?>
-      <hgroup id="site-name-wrap">
-        <h1 id="site-name">
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-            <span><?php print $site_name; ?></span>
-          </a>
-        </h1>
-        <?php if ($site_slogan): ?><h2 id="site-slogan"><?php print $site_slogan; ?></h2><?php endif; ?>
-      </hgroup>
-    <?php endif; ?>
-    
-  </header>
+  <div id="shadow-wrapper">
+    <header id="header" class="clearfix">
+      <?php if (theme_get_setting('image_logo','kunst_theme')): ?>
+        <?php if ($logo): ?><div id="site-logo"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+        <?php print render($page['header']); ?></div><?php endif; ?>
+      <?php else: ?>
+        <hgroup id="site-name-wrap">
+          <h1 id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+              <span><?php print $site_name; ?></span>
+            </a>
+          </h1>
+          <?php if ($site_slogan): ?><h2 id="site-slogan"><?php print $site_slogan; ?></h2><?php endif; ?>
+        </hgroup>
+      <?php endif; ?>
 
-  <div id="main" class="clearfix">
-    <div id="primary">
-      <section id="content" role="main">
-        <?php print $messages; ?>
-        <?php if ($page['content_top']): ?><div id="content_top"><?php print render($page['content_top']); ?></div><?php endif; ?>
-          <?php if (theme_get_setting('breadcrumbs')): ?><?php if ($breadcrumb): ?><div id="breadcrumbs"><?php print $breadcrumb; ?></div><?php endif;?><?php endif; ?>
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?><h1 class="page-title"><?php print $title; ?></h1><?php endif; ?>
-          <?php print render($title_suffix); ?>
-          <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
-          <?php print render($page['help']); ?>
-          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-          <?php print render($page['content']); ?>
+    </header>
 
-      </section> <!-- /#main -->
+    <div id="main" class="clearfix">
+      <div id="primary">
+        <section id="content" role="main">
+          <?php print $messages; ?>
+          <?php if ($page['content_top']): ?><div id="content_top"><?php print render($page['content_top']); ?></div><?php endif; ?>
+            <?php if (theme_get_setting('breadcrumbs')): ?><?php if ($breadcrumb): ?><div id="breadcrumbs"><?php print $breadcrumb; ?></div><?php endif;?><?php endif; ?>
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?><h1 class="page-title"><?php print $title; ?></h1><?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+            <?php print render($page['content']); ?>
+
+        </section> <!-- /#main -->
+      </div>
+
     </div>
-
-  </div>
+  
 <?php if ($is_front): ?>
   <footer id="footer-bottom">
     <div id="footer-area" class="clearfix">
@@ -119,7 +121,7 @@
     </div>
 <?php endif; ?>  
        
-
+</div>
     <div id="footer-bottom" class="clearfix">
            <div class="clearfix">
       <?php if ($page['footer_first'] || $page['footer_second'] || $page['footer_third']): ?>

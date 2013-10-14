087e04e6057b005da7994344df98fce424267670
gorkha <anme@odense.dk>
2013-10-14 11:11:10 +0200

    Added: main menu icon-caret rotate on hover

--- js/menu.js
+++ js/menu.js
@@ -14,7 +14,7 @@
   });
   
   function addExpandable() {
-    var div = '<div class="expandable"><i class="icon-caret-down"> </i></div>';
+    var div = '<div class="expandable"><i class="icon-caret-right"> </i></div>';
     var $aArr = $("#block-menu-block-1 ul.menu > li");
     if($aArr.length === 0) $aArr = $("#rm-removed ul.menu > li");
     $aArr.each(function(i, a){
@@ -28,14 +28,15 @@
     if($(window).width() > 767) {
       $("#block-menu-block-1 ul.menu > li").mouseenter(function(e) {
         $(this).find("> ul > li").stop(true, true).slideDown(100);
+        $(this).find("> a > div > i").attr("class", "icon-caret-down");
       });
       $("#block-menu-block-1 ul.menu > li").mouseleave(function(e) {
         $(this).find("ul li").stop(true, true).slideUp(100);
+        $(this).find("> a > div > i").attr("class", "icon-caret-right");
       });
-      //console.log("Wide");
     }
     else {
-      //console.log("Mobile");
+      // Mobile
       $("#block-menu-block-1 ul.menu > li").unbind();
       $("#rm-removed li").attr("style","");
     }

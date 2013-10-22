4f20499f69e92b6039d9f2fcf67bf0582b8150fc
gorkha <anme@odense.dk>
2013-10-22 14:31:44 +0200

    Removed duplicate IDs

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -66,21 +66,22 @@
   <div id="shadow-wrapper">
     <header id="header" class="clearfix">
       <?php if (theme_get_setting('image_logo','kunst_theme')): ?>
-        <?php if ($logo): ?><div id="site-logo"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-        <?php print render($page['header']); ?></div><?php endif; ?>
+        <?php if ($logo): ?>
+        <div id="site-logo">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php print render($page['header']); ?>
+        </div>
+        <?php endif; ?>
       <?php else: ?>
         <hgroup id="site-name-wrap">
           <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-              <span><?php print $site_name; ?></span>
-            </a>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><span><?php print $site_name; ?></span></a>
           </h1>
           <?php if ($site_slogan): ?><h2 id="site-slogan"><?php print $site_slogan; ?></h2><?php endif; ?>
         </hgroup>
       <?php endif; ?>
-
     </header>
 
     <div id="main" class="clearfix">
@@ -88,96 +89,82 @@
         <section id="content" role="main">
           <?php print $messages; ?>
           <?php if ($page['content_top']): ?><div id="content_top"><?php print render($page['content_top']); ?></div><?php endif; ?>
-            <?php if (theme_get_setting('breadcrumbs')): ?><?php if ($breadcrumb): ?><div id="breadcrumbs"><?php print $breadcrumb; ?></div><?php endif;?><?php endif; ?>
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?><h1 class="page-title"><?php print $title; ?></h1><?php endif; ?>
-            <?php print render($title_suffix); ?>
-            <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
-            <?php print render($page['help']); ?>
-            <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-            <?php print render($page['content']); ?>
-
+          <?php if (theme_get_setting('breadcrumbs')): ?><?php if ($breadcrumb): ?><div id="breadcrumbs"><?php print $breadcrumb; ?></div><?php endif;?><?php endif; ?>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?><h1 class="page-title"><?php print $title; ?></h1><?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          <?php print render($page['content']); ?>
         </section> <!-- /#main -->
       </div>
-
     </div>       
   </div>
 
   <?php if ($is_front): ?>
-    <div id="shadow-bottom">
-      <footer id="footer-bottom">
+    <div id="shadow-bottom-front">
+      <div id="footer-bottom-front">
         <div id="footer-area" class="clearfix">
           <?php if ($page['bottom_first'] || $page['bottom_second'] || $page['bottom_third']): ?>
             <div id="bottom-block-wrap" class="clearfix in<?php print (bool) $page['bottom_first'] + (bool) $page['bottom_second'] + (bool) $page['bottom_third']; ?>">
               <?php if ($page['bottom_first']): ?><div class="bottom-block">
-                  <?php print render($page['bottom_first']); ?>
+              <?php print render($page['bottom_first']); ?>
                 </div><?php endif; ?>
               <?php if ($page['bottom_second']): ?><div class="bottom-block">
-                  <?php print render($page['bottom_second']); ?>
+              <?php print render($page['bottom_second']); ?>
                 </div><?php endif; ?>
               <?php if ($page['bottom_third']): ?><div class="bottom-block">
-                  <?php print render($page['bottom_third']); ?>
+              <?php print render($page['bottom_third']); ?>
                 </div><?php endif; ?>
             </div>
           <?php endif; ?>
-        </div></div>
+        </div>
+      </div>
+    </div>
   <?php endif; ?>
 
-
-
-  <div id="shadow-bottom">
+  <footer id="shadow-bottom">
     <div id="footer-bottom" class="clearfix">
-           <div class="clearfix">
+      <div class="clearfix">
       <?php if ($page['footer_first'] || $page['footer_second'] || $page['footer_third']): ?>
         <div id="footer-block-wrap" class="clearfix in<?php print (bool) $page['footer_first'] + (bool) $page['footer_second'] + (bool) $page['footer_third']; ?>">
-          <?php if($page['footer_first']): ?><div class="footer-block">
-            <?php print render($page['footer_first']); ?>
-          </div><?php endif; ?>
-          <?php if($page['footer_second']): ?><div class="footer-block">
-            <?php print render($page['footer_second']); ?>
-          </div><?php endif; ?>
-          <?php if($page['footer_third']): ?><div class="footer-block">
-            <?php print render($page['footer_third']); ?>
-          </div><?php endif; ?>
-          <?php if($page['footer_fourth']): ?><div class="footer-block" >
-             <?php print render($page['footer_fourth']); ?>
-          </div><?php endif; ?>
+          <?php if($page['footer_first']): ?>
+            <div class="footer-block"><?php print render($page['footer_first']); ?></div>
+          <?php endif; ?>
+          <?php if($page['footer_second']): ?>
+            <div class="footer-block"><?php print render($page['footer_second']); ?></div>
+          <?php endif; ?>
+          <?php if($page['footer_third']): ?>
+            <div class="footer-block"><?php print render($page['footer_third']); ?></div>
+          <?php endif; ?>
+          <?php if($page['footer_fourth']): ?>
+            <div class="footer-block" ><?php print render($page['footer_fourth']); ?></div>
+          <?php endif; ?>
         </div>
       <?php endif; ?>
-          </div>
+      </div>
     </div>
-
   </footer>
+  
   <div id="bottom" class="clearfix">
     <?php if (theme_get_setting('socialicon_display', 'kunst_theme')): ?>
-        <?php 
-        $twitter_url = check_plain(theme_get_setting('twitter_url', 'kunst_theme')); 
-        $facebook_url = check_plain(theme_get_setting('facebook_url', 'kunst_theme')); 
-        ?>
-      <div class="social-profile">
-        <ul>
-          <?php if ($facebook_url): ?>
-          <li><a target="_blank" title="<?php print $site_name; ?> in Facebook" href="<?php print $facebook_url; ?>">
-          <i class="icon-facebook-sign" ></i>
-          </a>
-            </li>
-          <?php endif; ?>
-          <?php if ($twitter_url): ?>
-          <li><a target="_blank" title="<?php print $site_name; ?> in Twitter" href="<?php print $twitter_url; ?>"><i class="icon-twitter-sign" > </i></a></li>
-          <?php endif; ?>
-          <li>
-            <a target="_blank" title="<?php print $site_name; ?> in RSS" href="<?php print $front_page; ?>rss.xml"> <i class="icon-rss-sign" > </i></a>
-          </li>
-        </ul>
-      </div>
-<?php endif; ?>
-      <div class="credit">Kunstportal Fyn er et samarbejdsprojekt mellem de fynske folkebiblioteker støttet af Kulturregion Fyn · Om Kunstportalfyn.dk · Kontakt os</div>
+    <?php 
+    $twitter_url = check_plain(theme_get_setting('twitter_url', 'kunst_theme')); 
+    $facebook_url = check_plain(theme_get_setting('facebook_url', 'kunst_theme')); 
+    ?>
+    <div class="social-profile">
+      <ul>
+        <?php if ($facebook_url): ?>
+        <li><a target="_blank" title="<?php print $site_name; ?> in Facebook" href="<?php print $facebook_url; ?>"><i class="icon-facebook-sign" ></i></a></li>
+        <?php endif; ?>
+        <?php if ($twitter_url): ?>
+        <li><a target="_blank" title="<?php print $site_name; ?> in Twitter" href="<?php print $twitter_url; ?>"><i class="icon-twitter-sign" > </i></a></li>
+        <?php endif; ?>
+        <li><a target="_blank" title="<?php print $site_name; ?> in RSS" href="<?php print $front_page; ?>rss.xml"> <i class="icon-rss-sign" > </i></a></li>
+      </ul>
     </div>
+    <?php endif; ?>
+    <div class="credit">Kunstportal Fyn er et samarbejdsprojekt mellem de fynske folkebiblioteker støttet af Kulturregion Fyn · Om Kunstportalfyn.dk · Kontakt os</div>
   </div>
 </div>
-
-
-
-
-
-
